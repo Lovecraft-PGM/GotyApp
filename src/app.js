bfd82e8152774d7028591f3d5fa7fef1576f1630
Maya <mayra.marulanda@hotmail.com>
2025-02-25 20:45:06 +1030

    Se agrega try y catch authcontroller

--- src/app.js
+++ src/app.js
@@ -4,7 +4,9 @@ import authRoutes from "./routes/auth.routes.js"
 
 const app = express();
 app.use(morgan('dev'));
-app.use(express.json) //Convierte el req.body en Json 
+
+app.use(express.json()) //Convierte el req.body en Json 
+
 app.use("/api",authRoutes);
 
 export default app;
