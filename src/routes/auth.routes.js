4fa23a2467ea4fe3e736ea99f1800da7ebf2c552
lovecraft-PGM <devlovecraft@gmail.com>
2025-02-27 22:04:37 -0500

    Se crear la validacion de JWT y se guarda el token en la cookie

--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
-import { register,login } from "../controllers/authControllers.js";
+import { register,login,logout } from "../controllers/authControllers.js";
 import { validateSchema } from "../middlewares/validatorMiddleware.js"
 import { registerSchema,loginSchema } from "../schemas/authSchemas.js"
 
 const router = Router();
 router.post("/register",validateSchema(registerSchema),register);
 router.post("/login",validateSchema(loginSchema),login);
+router.post("/logout",logout);
 
 export default router 
