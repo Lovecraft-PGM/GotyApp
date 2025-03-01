27c9950e81c6a02c0cb7f2c5cef9d8935b362403
lovecraft-PGM <devlovecraft@gmail.com>
2025-02-28 22:30:12 -0500

    Login y register funcionales con JWT

--- src/controllers/authControllers.js
+++ src/controllers/authControllers.js
@@ -1,7 +1,7 @@
-// import { JsonWebTokenError } from "jsonwebtoken";
-// import { TOKEN_SECRET } from "../config.js";
-import { createAccessToken } from "../libs/jwt.js";
 import User from "../models/userModel.js";
+import JWT from "jsonwebtoken";
+import { TOKEN_SECRET } from "../config.js";
+import { createAccessToken } from "../libs/jwt.js";
 import bcrypt from "bcryptjs"
 
 
@@ -20,15 +20,15 @@ export const register = async (req, res) => {
         });
 
         const userSaved = await newUser.save();
-        const token = await createAccessToken({id:userSaved._id});
-        res.cookie("token",token)
+        const token = await createAccessToken({ id: userSaved._id });
+        res.cookie("token", token)
         res.json({
             id: userSaved._id,
             username: userSaved.username,
             email: userSaved.email,
         });
     } catch (error) {
-        res.status(500).json({ message:error, message});
+        res.status(500).json({ message: error, message });
     }
 };
 
@@ -40,28 +40,43 @@ export const login = async (req, res) => {
 
         const isMatch = await bcrypt.compare(password, userFound.password);
         if (!isMatch) return res.status(400).json(["La contraseña es incorrecta"]);
-
-        const token = await createAccessToken({id:userSaved._id});
-        res.cookie("token",token)
+        
+        const token = await createAccessToken({ id: userFound._id });
+        res.cookie("token", token)
         res.json({
             id: userFound._id,
             username: userFound.username,
             email: userFound.email
         });
     } catch (error) {
+        console.log("aaa",error);
         res.status(500).json({ menssage: error.menssage });
     }
 };
 
 
-export const logout = async (req,res) =>{
-    res.cookie("token","",{
-        expires:new Date(0)
+export const logout = async (req, res) => {
+    res.cookie("token", "", {
+        expires: new Date(0)
     })
     return res.sendStatus(200)
 };
-// --- Aqui vamos
-// export const verifyToken = async (req,res) =>{
-//     const {token} = req.cookies
-// };
 
+export const verifyToken = async (req, res) => {
+    const { token } = req.cookies
+    if (!token) return res.status(401).json({ message: "Unauthorized" });
+    JWT.verify(token, TOKEN_SECRET, async(error, user) => {
+
+        if (error) return res.status(401).json({ message: "Unauthorized" });
+        const userFound = await User.findById(user.id);
+        
+        if (!userFound) return res.status(401).json({ message: "Unauthorized" });
+        return res.json({
+            id:userFound._id,
+            username:userFound.username,
+            email:userFound.email,
+        });
+    });
+};
+
+// export 
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,11 +1,13 @@
 import { Router } from "express";
-import { register,login,logout } from "../controllers/authControllers.js";
+import { register,login,logout, verifyToken } from "../controllers/authControllers.js";
+import {authRequired} from "../middlewares/validateToken.js"
 import { validateSchema } from "../middlewares/validatorMiddleware.js"
 import { registerSchema,loginSchema } from "../schemas/authSchemas.js"
 
 const router = Router();
 router.post("/register",validateSchema(registerSchema),register);
 router.post("/login",validateSchema(loginSchema),login);
+router.get("/vertify",verifyToken)
 router.post("/logout",logout);
 
 export default router 
